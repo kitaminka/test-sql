0d5f634e42a18997127690671976d59bb88164e2
kitaminka <kit4minka@gmail.com>
2023-10-11 08:50:51 +0300

    Update insert_from_array.sql

--- insert_from_array.sql
+++ insert_from_array.sql
@@ -1,7 +1,7 @@
 DO $$
 DECLARE
   companies text[]:= array['Folik', 'Arctiier Armstech', 'Adcor Defense', 'Arsenal, Inc', 'Izhmekh',
-						  'BPI Outdoors', 'Beretta'];
+						  'BPI Outdoors', 'Viuviu'];
   company text;
 BEGIN
   FOREACH company IN ARRAY companies LOOP
